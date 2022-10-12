875d479c11176dcae48e6cc33a47b76b23c69a05
m01nak <m01nak77@gmail.com>
2022-10-12 22:43:32 +0530

    add mock tests

--- src/test/java/com/moinak/mockito/DataProcessorMockTest.java
+++ src/test/java/com/moinak/mockito/DataProcessorMockTest.java
@@ -29,5 +29,16 @@ class DataProcessorMockTest {
 		DataProcessor dataProcessor = new DataProcessor(dataSupplyMock);
 		assertEquals(10.0, dataProcessor.max(),0.0000000001);
 	}
+	
+	@Test
+	void maxTest_multiElementArray() {
+		
+		int[] multiElementArray = new int[] {3,5,10,9,16,11,1,2,3,33};
+		
+		DataSupply dataSupplyMock = mock(DataSupply.class);
+		when(dataSupplyMock.getData()).thenReturn(multiElementArray);
+		DataProcessor dataProcessor = new DataProcessor(dataSupplyMock);
+		assertEquals(33, dataProcessor.max(),0.0000000001);
+	}
 
 }
